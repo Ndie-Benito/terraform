adf410bc76c564321f3bfacc9919c17dfc6fd2f4
Ndie-Benito <109445998+Ndie-Benito@users.noreply.github.com>
2025-02-02 16:44:53 -0500

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,8 +1,8 @@
 resource "azurerm_resource_group" "mcit420zz5um" {
-  name     = "mcit_resource_group_w24"
+  name     = "mcit_resource_group"
   location = "canadacentral"
 }
 resource "azurerm_resource_group" "mcit420zz5um2" {
-  name     = "mcit_resource_group_w24_2"
+  name     = "mcit_resource_group47"
   location = "canadacentral"
 }
